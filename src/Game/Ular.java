2ff0dbafe7bc7c3bb214eaa2aac04ae35d2a4bc6
ArnethaNirwanaPutri <arnethanirwana1234@gmail.com>
2024-12-14 09:10:30 +0800

    membuat kode untuk mengatur posisi ular dan mengakses gambar ular yang berada pada foder res (F1D022113)

--- src/Game/Ular.java
+++ src/Game/Ular.java
@@ -1,5 +1,20 @@
 package Game;
 
+import java.awt.Graphics;
+import java.awt.Image;
+import javax.swing.ImageIcon;
+
 public class Ular {
-    
+
+    public static void drawUlar(Graphics g) {
+        Image ular2 = new ImageIcon("res/Papan/Ular/ular2.png").getImage();
+        Image ular3 = new ImageIcon("res/Papan/Ular/ular3.png").getImage();
+        Image ular4 = new ImageIcon("res/Papan/Ular/ular4.png").getImage();
+        Image ular5 = new ImageIcon("res/Papan/Ular/ular5.png").getImage();
+
+        g.drawImage(ular2, 25, 250, 150, 200, null);
+        g.drawImage(ular3, 130, 5, 120, 180, null);
+        g.drawImage(ular4, 400, 325, 140, 160, null);
+        g.drawImage(ular5, 250, 115, 100, 120, null);
+    }
 }
